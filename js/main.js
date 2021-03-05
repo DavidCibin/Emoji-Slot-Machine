22ee10f8a9a23719691d49adc1ade0931464e93e
David Cibin <davidisurf@hotmail.com>
2021-03-05 16:08:52 -0500

    add cached elements && eventListeners

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 
 
 /*------Variables (state)------*/
+// Variables might include (board/turn/winner)
 let totalPoints = 200
 
 let reel1 = 0
@@ -10,16 +11,26 @@ let reel2 = 5
 let reel3 = 10
 
 
-// Variables might include (board/turn/winner)
+
 
 /*------Cached Element References------*/
+// Cached Element References
+let bet1 = document.getElementById('bet1')
+let bet2 = document.getElementById('bet2')
+let bet3 = document.getElementById('bet3)
+
 
 // You might choose to put your game status here
 
 /*------Event Listeners------*/
-
 // This is where you should put the event listener
 // for a mouse-click
+spinBtn.addEventListener('click', spinClick)
+bet1.addEventListener('click', bet1Click)
+bet2.addEventListener('click', bet2Click)
+bet3.addEventListener('click', bet3Click)
+
+
 
 /*------Functions------*/
 
@@ -34,8 +45,8 @@ function getPoints() {
     console.log(points)
 }
 
-totalPoints += getPoints() 
-console.log('My total points: ' + getPoints())
+// totalPoints += getPoints() 
+// console.log('My total points: ' + getPoints())
 
 // Some functions you might choose to use:
 
