15d1e2d1f4295e46ee58bbd8dacaf981a99194f8
David Cibin <davidisurf@hotmail.com>
2021-03-05 19:55:27 -0500

    fix eventListeners, console.log tests

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
 /*------Constants------*/
-
-
-
+const emojis = {
+    'a': 0,     //😱
+    'b': 0,     //😭
+    'c': 0,     //😠 
+    'd': 5,     //😃
+    'e': 10,    //🤑
+    'f': 25,    //💎
+    'g': 50,    //🎰
+}
+console.log(emojis)
+console.log(Object.keys[2]);
 /*------Variables (state)------*/
 // Variables might include (board/turn/winner)
-let totalPoints = 200
-
-let reel1 = 0
-let reel2 = 5
-let reel3 = 10
-
+let status = document.getElementById("status")
+let totalPoints = 0
 
 
 
 /*------Cached Element References------*/
 // Cached Element References
-let bet1 = document.getElementById('bet1')
-let bet2 = document.getElementById('bet2')
-let bet3 = document.getElementById('bet3)
+const slots = document.querySelectorAll('.reels > div');
+console.log(slots)
+// let bet1 = document.getElementById('bet1')
+// let bet2 = document.getElementById('bet2')
+// let bet3 = document.getElementById('bet3')
 
 
 // You might choose to put your game status here
@@ -25,26 +31,59 @@ let bet3 = document.getElementById('bet3)
 /*------Event Listeners------*/
 // This is where you should put the event listener
 // for a mouse-click
-spinBtn.addEventListener('click', spinClick)
-bet1.addEventListener('click', bet1Click)
-bet2.addEventListener('click', bet2Click)
-bet3.addEventListener('click', bet3Click)
-
+document.getElementById('spinBtn').addEventListener('click', spinClick);
+document.getElementById('bet1').addEventListener('click', bet1Click);
+document.getElementById('bet2').addEventListener('click', bet2Click);
+document.getElementById('bet3').addEventListener('click', bet3Click);
+console.log(bet1Click)
 
 
 /*------Functions------*/
+function spinClick(evt) {       //Function to spin the reels and a random number for each reel
+    status.innerHTML = "SPINNING"
+    console.log(evt.target.id)    
+}
+
+function bet1Click(evt) {       
+    //Function to multiply the bet by 1x(original bet)
+    //Function to multiply the bet by 3x(original bet)
+    //if winner totalPoints = + (bet * 1)
+    //else totalPoints = - (bet * 1)
+}
+
+function bet2Click(evt) {       
+    //Function to multiply the bet by 3x
+    //if winner totalPoints = + (bet * 2)
+    //else totalPoints = - (bet * 2)
+}
+
+function bet3Click(evt) {       
+    //Function to multiply the bet by 3x
+    //if winner totalPoints = + (bet * 3)
+    //else totalPoints = - (bet * 3)
+}
+
+
+function init() {
+    //Here is how the game will start
+    // totalPoints set to 100 for new game
+    totalPoints = 100
+    // console.log('total points in ' ,totalPoints)
+    status.innerHTML = "SPINNING"
+}
+console.log('total points out ' ,totalPoints)
 
 function getPoints() {
-    let points = 0
+    let points = 20
     if ((reel1 + reel2 + reel3) < 10) {
         points -= 5
     }
     else {
         points = (reel1 + reel2 + reel3)
     }
-    console.log(points)
+    console.log(getPoints())
 }
-
+console.log(getPoints());
 // totalPoints += getPoints() 
 // console.log('My total points: ' + getPoints())
 
@@ -66,6 +105,13 @@ function getPoints() {
 
 
 // Render function:
-// Displays the current state of the board
+// Displays the current points
 // on the page, updating the elements to reflect
 // either X or O depending on whose turn it is
+function render() {
+    
+}
+
+
+
+ init();
