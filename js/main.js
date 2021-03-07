c8973334b4b4fb698338fa9c7e4b9a6e3b8e2e4f
David Cibin <davidisurf@hotmail.com>
2021-03-06 20:46:30 -0500

    add audio, fix some variables, scores adjustments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 /*------Constants------*/
+const audioTest = new Audio('audio/jackpot.wav');
+// const jackpot = new Audio('audio/jackpot.wav');
+const winMatch = new Audio('audio/win.wav');
+
+// const emojis = {
+//     '😱': 0,     //😱
+//     '😭': 0,     //😭
+//     '😠': 0,     //😠 
+//     '😃': 5,     //😃
+//     '🤑': 10,    //🤑
+//     '💎': 25,    //💎
+//     '🎰': 50,    //🎰
+// }
+
 const emojis = {
     'a': 0,     //😱
     'b': 0,     //😭
@@ -9,23 +23,30 @@ const emojis = {
     'g': 50,    //🎰
 }
 console.log(emojis)
-console.log(Object.keys[emojis[1]]);
+console.log(emojis.d+emojis.d)
+
 /*------Variables (state)------*/
-// Variables might include (board/turn/winner)
-let totalPoints = 0
-let reel1 = 6
+let totalPoints = 100;
+let points = 0;
+let reel1 = 50;
+let reel2 = 50;
+let reel3 = 50;
+let match = null
+let sound = true;
+
 let game = false 
 
 
 
 /*------Cached Element References------*/
 // Cached Element References
-const slots = document.querySelectorAll('.reels > div');
-let status = document.getElementById("status")
-console.log(slots)
-// let bet1 = document.getElementById('bet1')
-// let bet2 = document.getElementById('bet2')
-// let bet3 = document.getElementById('bet3')
+const slot1 = document.getElementById('reel1');
+const slot2 = document.getElementById('reel2');
+const slot3 = document.getElementById('reel3');
+let score = document.getElementById('points');
+let status = document.getElementById('status')
+let audio = document.getElementById('audio');
+console.log(status)
 
 
 // You might choose to put your game status here
@@ -34,106 +55,109 @@ console.log(slots)
 // This is where you should put the event listener
 // for a mouse-click
 document.getElementById('spinBtn').addEventListener('click', spinClick);
-document.getElementById('bet1').addEventListener('click', bet1Click);
-document.getElementById('bet2').addEventListener('click', bet2Click);
-document.getElementById('bet3').addEventListener('click', bet3Click);
-console.log(bet1Click)
+document.getElementById('audio').addEventListener('click', toggleAudio);
+document.getElementById('spinBtn').addEventListener('click', init) //When the game is over
+console.log('spin click ', spinClick)
+console.log('spin click ', toggleAudio)
+
 
 
 /*------Functions------*/
-function drawSlot1() {
-	var slot1Value = document.getElementById('slotImage1');
-	var r1 = Math.floor(Math.random() * 9);
-	slot1Value.src = "graphics/" + fruitArray[r1] + ".jpg";
-    console.log(r1)
-}
-// console.log(r1)
+
+// On-Click function:
+// generate random "numbers"
+function spinClick() {       //Function to spin the reels and a random number for each reel
+    return status.innerText = 'SPINNING';
+  
+//     if (points > 0) {
+//     status.innerHTML = "SPINNING"
+//     console.log(reel1)    
+//     return reel1 = math.random(7) 
+//     console.log(reel1)
+//    }
+// ((reel1 + reel2 + reel3) < 10 no match
+// ((reel1 + reel2 + reel3) >= 10 && < 150 we have a match
+// ((reel1 + reel2 + reel3) = 150 jackpot
 
 
-function spinClick(evt) {       //Function to spin the reels and a random number for each reel
-   if (points > 0) {
-    status.innerHTML = "SPINNING"
-    console.log(reel1)    
-    return reel1 = math.random(7) 
-    console.log(reel1)
-   }
-}
-console.log(reel1)
-
-function bet1Click(evt) {       
-    //Function to multiply the bet by 1x(original bet)
-    //Function to multiply the bet by 3x(original bet)
-    //if winner totalPoints = + (bet * 1)
-    //else totalPoints = - (bet * 1)
-}
-
-function bet2Click(evt) {       
-    //Function to multiply the bet by 3x
-    //if winner totalPoints = + (bet * 2)
-    //else totalPoints = - (bet * 2)
-}
-
-function bet3Click(evt) {       
-    //Function to multiply the bet by 3x
-    //if winner totalPoints = + (bet * 3)
-    //else totalPoints = - (bet * 3)
 }
 
 
-function init() {   // Initialization function:
-    
+function init() {   //Initialization function:
+    //Only start new game when totalPoints = 0
     //Here is how the game will start
     // totalPoints set to 100 for new game
     totalPoints = 100
-    // console.log('total points in ' ,totalPoints)
+    points = 20
+    render()
+    slot1.src = emojis.c 
+    slot2.src = emojis.d
+    slot3.src = emojis.f
+}
+
+
+// Function to check for matchs and handle points
+function getPoints() {
+   
     
 }
-console.log('total points out ' ,totalPoints)
-
-function getPoints() {
-    let points = 0
-    if ((reel1 + reel2 + reel3) < 10) {
-        points -= 5
-        status.innerHTML = "No Match!"
-        //PLAY LOSE AUDIO?? MAYBE NOT
-    }
-    else if ((reel1 + reel2 + reel3) == 150) {
-        status.innerHTML = "JACKPOT!"
-        //PLAY JACKPOT AUDIO
-    }
-    else {
-        points = (reel1 + reel2 + reel3)
-        status.innerHTML = "You Win!"
-        //PLAY WIN AUDIO
-    }
-    // console.log(getPoints())
-}
-console.log(getPoints()); //Undefined until I get the points to >> reels
-
-// totalPoints += getPoints() 
-// console.log('My total points: ' + getPoints())
-
-// Some functions you might choose to use:
-
-
-// On-Click function:
-// Set up what happens when one of the elements
-// is clicked
-
-
-// Check winner function:
-// Checks the current state of the board for
-// a winner and changes the state of the winner
-// variable if so
-
-
 
 
 function render() { // Render function:
     // Displays the current points
     // Game over = no points
-    
+    let points = 0;
+    if (jackpot) {
+        
+        totalPoints += 150;
+        status.innerText = "✰ ✰ ✰ ✰ ✰ ✰ JACKPOT ✰ ✰ ✰ ✰ ✰ ✰";
+        points.innerText = "150"; //CAN I DISPLAY THIS IN THE SCORE FOR LIKE 1 SECOND?
+        // jackpot.play();
+        
+    }
+    else if (match) {
+        points = (reel1 + reel2 + reel3);
+        totalPoints += points
+        status.innerText = "YOU WIN!"
+        points.innerText = "points"; //CAN I DISPLAY THIS IN THE SCORE FOR LIKE 1 SECOND?
+        // win.play();
+        
+    }
+    else if {
+        ((reel1 + reel2 + reel3) < 10) 
+        totalPoints -= 5;
+        if(totalPoints > 0) {
+            status.innerText = "SPIN AGAIN";
+        } else {
+            status.innerText = "😭 GAME OVER 😭";
+        }
+        
+        points.innerText = "-5"; //CAN I DISPLAY THIS IN THE SCORE FOR LIKE 1 SECOND?
+        //lose.play();    PLAY LOSE AUDIO?? PROBABLY
+    }
+    else
 
+
+    getPoints();
+    // Here we will handle the points, messages for win, lose, jackpot
+
+}
+
+
+
+// Function for audio effects. Toggle on/off
+function toggleAudio(){  //togle icon works, play/plause needs adjust to all sounds
+    if (!sound){
+        sound = true;
+        audioTest.play();
+        audio.innerHTML = '<img src="images/audioOn.png">';
+    } 
+    else{
+        sound = false;
+        audioTest.pause();
+        audio.innerHTML = '<img src="images/audioOff.png">';
+    }
+    
 }
 
 
