fe3a57d742a14433d1678b6a6ca6623c43804399
David Cibin <davidisurf@hotmail.com>
2021-03-08 13:11:20 -0500

    Reels display emojis randomly

--- js/main.js
+++ js/main.js
@@ -1,177 +1,201 @@
 /*------Constants------*/
 const spin = new Audio('audio/spin.mp3');
-const audioTest = new Audio('audio/jackpot.wav');
-// const jackpot = new Audio('audio/jackpot.wav');
-const winMatch = new Audio('audio/win.wav');
+const jackpot = new Audio('audio/jackpot.wav');
+const win = new Audio('audio/win.wav');
+const lose = new Audio('audio/lose.wav');
 
-// const emojis = {
-//     '😱': 0,     //😱
-//     '😭': 0,     //😭
-//     '😠': 0,     //😠 
-//     '😃': 5,     //😃
-//     '🤑': 10,    //🤑
-//     '💎': 25,    //💎
-//     '🎰': 50,    //🎰
-// }
+const scoreCard = [
+    { emojiValue: 0, emoji: '😱', score: 0 },
+    { emojiValue: 1, emoji: '😭', score: 0 },
+    { emojiValue: 2, emoji: '😠', score: 0 },
+    { emojiValue: 3, emoji: '😃', score: 5 },
+    { emojiValue: 4, emoji: '🤑', score: 10 },
+    { emojiValue: 5, emoji: '💎', score: 25 },
+    { emojiValue: 6, emoji: '🎰', score: 50 },
+]
+const start = '🎰'
 
-const emojis = {
-    'a': 0,     //😱
-    'b': 0,     //😭
-    'c': 0,     //😠 
-    'd': 5,     //😃
-    'e': 10,    //🤑
-    'f': 25,    //💎
-    'g': 50,    //🎰
-}
-console.log(emojis)
-console.log(emojis.d+emojis.d)
+
 
 /*------Variables (state)------*/
-let totalPoints = 100;
+let totalPoints = 0;
 let points = 0;
-let reel1 = 50;
-let reel2 = 50;
-let reel3 = 50;
+let reel1 = null;
+let reel2 = null;
+let reel3 = null;
 let match = null
-let sound = true;
-
-let game = false 
-
+let sound = false;
 
 
 /*------Cached Element References------*/
-// Cached Element References
 const slot1 = document.getElementById('reel1');
 const slot2 = document.getElementById('reel2');
 const slot3 = document.getElementById('reel3');
 let score = document.getElementById('points');
 let status = document.getElementById('status')
 let audio = document.getElementById('audio');
-console.log(status)
 
-
-// You might choose to put your game status here
 
 /*------Event Listeners------*/
-// This is where you should put the event listener
-// for a mouse-click
 document.getElementById('spinBtn').addEventListener('click', spinClick);
 document.getElementById('audio').addEventListener('click', toggleAudio);
 document.getElementById('spinBtn').addEventListener('click', init) //When the game is over
-console.log('spin click ', spinClick)
-console.log('spin click ', toggleAudio)
-
 
 
 /*------Functions------*/
+//Function to spin the reels and a random number for each reel
+function spinClick() {
+    document.getElementById('spinBtn').style.pointerEvents = 'none' //Block SPIN button for been pressed during spinning
+    let currentTime = 0;
+    let interval = 250;     //Add interval to get a random number every 0.25 second
+    let maxTime = 4500;      //For a total time of 4.5 seconds
+    // console.log('START')
+    let slotInterval = setInterval(function () {
+        // console.log('Interval ==> ', currentTime)
+        if (currentTime < maxTime) {
 
-// On-Click function:
-// generate random "numbers"
-function spinClick() {       //Function to spin the reels and a random number for each reel
-    let rand1 = Math.floor(Math.random() * (7 - 0 + 1)) + 0;
-    reel1 = rand1;
-    let rand2 = Math.floor(Math.random() * (7 - 0 + 1)) + 0;
-    reel2 = rand2;
-    let rand3 = Math.floor(Math.random() * (7 - 0 + 1)) + 0;
-    reel3 = rand3;
-    spin.play()
-    slot1.innerText = reel1;
-    slot2.innerText = reel2;
-    slot3.innerText = reel3;
-    status.innerText = 'SPINNING';
+            currentTime += interval
+            reel1 = Math.floor(Math.random() * (6 - 0 + 1)) + 0;
+            reel2 = Math.floor(Math.random() * (6 - 0 + 1)) + 0;
+            reel3 = Math.floor(Math.random() * (6 - 0 + 1)) + 0;
+            console.log('reeelnums',reel1, reel2, reel3); //debug random numbers
+            
+            //Assign the emoji to the random number from reel1 using the function findEmoji
+            let reel1EmojiObject = findEmoji(reel1);
+            slot1.innerText = reel1EmojiObject[0].emoji
+            // points += reel1EmojiObject[0].score // will not be used here
 
+            //Assign the emoji to the random number from reel2 using the function findEmoji
+            let reel2EmojiObject = findEmoji(reel2);
+            slot2.innerText = reel2EmojiObject[0].emoji
+            // points += reel2EmojiObject[0].score // will not be used here
+            
+            //Assign the emoji to the random number from reel3 using the function findEmoji
+            let reel3EmojiObject = findEmoji(reel3);
+            slot3.innerText = reel3EmojiObject[0].emoji
+            // points += reel3EmojiObject[0].score // will not be used here
 
-//     if (points > 0) {
-//     status.innerHTML = "SPINNING"
-//     console.log(reel1)    
-//     return reel1 = math.random(7) 
-//     console.log(reel1)
-//    }
-// ((reel1 + reel2 + reel3) < 10 no match
-// ((reel1 + reel2 + reel3) >= 10 && < 150 we have a match
-// ((reel1 + reel2 + reel3) = 150 jackpot
+            status.innerText = 'SPINNING';
+            score.innerText = totalPoints;
+            spin.play() //inactive while testing
 
-
+        } else {
+            // console.log('END')
+            document.getElementById('spinBtn').style.pointerEvents = 'auto' //Enable SPIN button back after spinning
+            clearInterval(slotInterval)
+            render()
+        }
+    }, interval)
 }
 
+function findEmoji(foundNum) {                              //foundNum is the parameter that correspond to each random reel
+    if (foundNum === null) {
+        return;
+    }
+    let emojiFound = scoreCard.filter(function (obj) {      //returns(creates) an array from the scoreBoard using the filter method
+        console.log('find emoji??', obj.emojiValue)         //log each emoji value from the scoreBoard
+        return obj.emojiValue === foundNum 
+    })
+    console.log('emoji found', emojiFound)                  //log each emoji value from the scoreBoard
+    return emojiFound;
+}
 
-function init() {   //Initialization function:
+//Initialization function:
+function init() {   
     //Only start new game when totalPoints = 0
     //Here is how the game will start
     // totalPoints set to 100 for new game
+    points = 0
     totalPoints = 100
-    points = 20
-    render()
-    slot1.innerText = 0;
-    slot2.innerText = 2;
-    slot3.innerText = 7;
+    slot1.innerText = scoreCard[6].emoji;
+    slot2.innerText = scoreCard[6].emoji;
+    slot3.innerText = scoreCard[6].emoji;
+    score.innerText = totalPoints
+   
 }
-
 
 // Function to check for matchs and handle points
 function getPoints() {
-   
     
+    /* Find a better way to account for points in here, now doing it in render function
+    let roll1 = findEmoji(reel1)    
+    let roll2 = findEmoji(reel2)    
+    let roll3 = findEmoji(reel3)
+    let rolledCombo = roll1[0].score + roll2[0].score + roll3[0].score; //get the total from each
+    let winningCombos = [5, 10, 15, 20, 30, 50, 75, 150] 
+    */
 }
 
 
 function render() { // Render function:
     // Displays the current points
     // Game over = no points
+    console.log(reel1, reel2, reel3)  //verification for the random numbers
+    console.log('verify my score:before all', totalPoints)  //verify my score:before all
+    
+
+    // Find a better way to account for points inside the getPoints function, to clear the render a little
+    let roll1 = findEmoji(reel1)    
+    let roll2 = findEmoji(reel2)    
+    let roll3 = findEmoji(reel3)
+    let rolledCombo = roll1[0].score + roll2[0].score + roll3[0].score; //get the total from each
+    let winningCombos = [5, 10, 15, 20, 30, 50, 75, 150] 
+    
+
     let points = 0;
-    if (jackpot) {
-        
+    if (rolledCombo == 150) {
+        console.log('verify my score:before jackpot', totalPoints)  //verify my score:before jackpot
         totalPoints += 150;
         status.innerText = "✰ ✰ ✰ ✰ ✰ ✰ JACKPOT ✰ ✰ ✰ ✰ ✰ ✰";
-        points.innerText = "150"; //CAN I DISPLAY THIS IN THE SCORE FOR LIKE 1 SECOND?
-        // jackpot.play();
-        
+        points.innerText = "150"; //Display points for 1 sec then display totalPoints??
+        jackpot.play();
+        console.log('verify my score:after jackpot', totalPoints)  //verify my score:after jackpot
+
     }
-    else if (match) {
-        points = (reel1 + reel2 + reel3);
-        totalPoints += points
-        status.innerText = "YOU WIN!"
-        points.innerText = "points"; //CAN I DISPLAY THIS IN THE SCORE FOR LIKE 1 SECOND?
-        // win.play();
-        
+    else if (winningCombos.includes(rolledCombo)) {
+        console.log('verify my score:before win', totalPoints)  //verify my score:before win
+        console.log(rolledCombo)
+        // points = (reel1 + reel2 + reel3);
+        totalPoints += rolledCombo//points
+        status.innerText = `YOU WIN! ${rolledCombo}`
+        score.innerText = `+${rolledCombo}`; //Display points for 1 sec then display totalPoints??
+        win.play();
+        console.log('verify my score:after win', totalPoints)  //verify my score:after win
+
     }
-    else if (lose) {
+    else {
+        console.log('verify my score:before lose', totalPoints)  //verify my score:before lose
         totalPoints -= 5;
-        if(totalPoints > 0) {
+        console.log('verify my score:after lose', totalPoints)  //verify my score:after lose
+        if (totalPoints > 0) {
             status.innerText = "SPIN AGAIN";
+            score.innerText = '-5'; //Display points for 1 sec then display totalPoints??
         } else {
             status.innerText = "😭 GAME OVER 😭";
         }
-        
-        points.innerText = "-5"; //CAN I DISPLAY THIS IN THE SCORE FOR LIKE 1 SECOND?
-        //lose.play();    PLAY LOSE AUDIO?? PROBABLY
+        lose.play();
     }
-    else
-
 
     getPoints();
     // Here we will handle the points, messages for win, lose, jackpot
 
 }
 
-
-
-// Function for audio effects. Toggle on/off
-function toggleAudio(){  //togle icon works, play/plause needs adjust to all sounds
-    if (!sound){
+//Function for audio effects. Toggle on/off
+function toggleAudio() {  //toggle icon works, play/plause needs adjust to all sounds
+    if (!sound) {
         sound = true;
-        audioTest.play();
         audio.innerHTML = '<img src="images/audioOn.png">';
-    } 
-    else{
+    }
+    else {
         sound = false;
-        audioTest.pause();
+        spin.pause();
+        win.pause();
+        lose.pause();
+        jackpot.pause();
         audio.innerHTML = '<img src="images/audioOff.png">';
     }
-    
+
 }
 
-spinClick()
-console.log(reel1)  
-
- init();
+init();
